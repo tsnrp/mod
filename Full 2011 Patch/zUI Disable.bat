e0d5f4020f5f0d1ab7b80ba13f7f2b6bbdbc5c74
XavierWise <49823332+XavierWise@users.noreply.github.com>
2019-11-11 22:43:12 +0000

    removed full

--- Full 2011 Patch/zUI Disable.bat
+++
@@ -1,5 +0,0 @@
- XCOPY "dat\tacShldA1Original.png" "dat\tacShldA1.png" /Y /I
- XCOPY "dat\tacShldA2Original.png" "dat\tacShldA2.png" /Y /I
- XCOPY "dat\tacShldA3Original.png" "dat\tacShldA3.png" /Y /I
- XCOPY "dat\scienceReticleOriginal.png" "dat\scienceReticle.png" /Y /I
-  XCOPY "dat\targetReticle2Original.png" "dat\targetReticle2.png" /Y /I
