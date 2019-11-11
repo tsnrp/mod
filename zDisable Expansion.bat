c1204c32ecc8a2fe893e1fab6561148f57056ac8
XavierWise <49823332+XavierWise@users.noreply.github.com>
2019-11-11 22:51:38 +0000

    all files for full mod added

---
+++ zDisable Expansion.bat
@@ -0,0 +1,16 @@
+ XCOPY "dat\Logo512Original.png" "dat\Logo512.png" /Y /I
+ 
+ XCOPY "dat\icon-nebulaOriginal.png" "dat\icon-nebula.png" /Y /I
+ XCOPY "dat\icon-blackholeOriginal.png" "dat\icon-blackhole.png" /Y /I
+ 
+ XCOPY "dat\vesselDataOriginal.xml" "dat\vesselData.xml" /Y /I
+ 
+ XCOPY "dat\overheat-warningOriginal.wav" "dat\overheat-warning.wav" /Y /I
+ 
+ XCOPY "artemisOriginal.ini" "artemis.ini" /Y /I
+ 
+ XCOPY "dat\tacShldA1Original.png" "dat\tacShldA1.png" /Y /I
+ XCOPY "dat\tacShldA2Original.png" "dat\tacShldA2.png" /Y /I
+ XCOPY "dat\tacShldA3Original.png" "dat\tacShldA3.png" /Y /I
+ XCOPY "dat\scienceReticleOriginal.png" "dat\scienceReticle.png" /Y /I
+ XCOPY "dat\targetReticle2Original.png" "dat\targetReticle2.png" /Y /I
